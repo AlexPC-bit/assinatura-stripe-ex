13f56e55ba818a06175c803fd4b8bfa4c6cd383c
AlexPC-bit <alexchavescontato@gmail.com>
2026-09-22 05:17:46 -0300

    carga-inicial-testes

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = ['name', 'stripe_price_id', 'price', 'interval'];
+
+    public function subscriptions(): HasMany
+    {
+        return $this->hasMany(Subscription::class);
+    }
 }
---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Plan;
+
+class PlanSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Plan::create([
+            'name' => 'Plano Básico',
+            'stripe_price_id' => 'price_1UHJEUJiJeZBmdShz5OyXnCz', 
+            'price' => 29.90,
+            'interval' => 'month',
+        ]);
+
+        Plan::create([
+            'name' => 'Plano Pro',
+            'stripe_price_id' => 'price_1UHJEtJiJeZBmdShyKavICT7', 
+            'price' => 59.90,
+            'interval' => 'month',
+        ]);
+    }
+}
